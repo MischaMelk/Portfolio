f626ca2a4758e3e30d9094f08a606e7f09bcb4e4
MischaMelk <mischa.melkert8@gmail.com>
2025-05-14 10:26:53 +0200

    update

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Hello, World!");
+
+document.querySelectorAll('[data-popup]').forEach(section => {
+    section.addEventListener('click', () => {
+      const popupId = section.getAttribute('data-popup');
+      document.getElementById(popupId).classList.add('active');
+    });
+  });
+  
+  document.querySelectorAll('.close-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.closest('.backdrop').classList.remove('active');
+    });
+  });
+  
+  // Optioneel: sluit popup bij klikken op de donkere achtergrond
+  document.querySelectorAll('.backdrop').forEach(backdrop => {
+    backdrop.addEventListener('click', (e) => {
+      if (e.target === backdrop) {
+        backdrop.classList.remove('active');
+      }
+    });
+  });
+
+  
+ 
+  
+
+  gsap.from('[aria-label="vaardigheden"] div ',{
+	duration:1,
+	x: 1000,
+	stagger:.3,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: '[aria-label="vaardigheden"]',
+        start: "top 70%", // wanneer het top van .vaardigheden 80% van het scherm bereikt
+        toggleActions: "play none none none" // alleen afspelen, geen reset
+      }});
